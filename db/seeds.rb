3c93c95d795c11a54acf2ba79bb4f672dd33181a
togashi29 <saladwithsalad@yahoo.co.jp>
2020-03-14 15:24:20 +0900

    rubocop -a

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,5 +25,5 @@ users = User.all
 user  = users.first
 following = users[2..50]
 followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+following.each {|followed| user.follow(followed) }
+followers.each {|follower| follower.follow(user) }
